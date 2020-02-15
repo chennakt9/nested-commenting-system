4924827d388871e1032565c5b2d75a4b471802a9
chennatiru <chenna.kesava@iitgn.ac.in>
2020-02-15 17:19:06 +0530

    change

--- main.js
+++ main.js
@@ -181,7 +181,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('./Frontend/build'));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname + 'Frontend' + 'build' + 'index.html'));
+        res.sendFile(path.join(__dirname + '/Frontend/build/index.html'));
     });
 }
 
